df958b88b19d3805ec08bd3720c972740c2433a0
quyetvu <quyetvu1299@gmail.com>
2023-10-12 21:13:48 +0700

    Init project

---
+++ src/main/java/com/example/luv/LuvApplication.java
@@ -0,0 +1,13 @@
+package com.example.luv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luv/LuvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
